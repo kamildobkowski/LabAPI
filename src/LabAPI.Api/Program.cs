6721d1d6b7c77d8985a93a3b7746529f5b98aacd
kamildobkowski <kamil.dobkowski.03@gmail.com>
2024-03-21 18:30:29 +0100

    Add Swagger Authentication

--- src/LabAPI.Api/Program.cs
+++ src/LabAPI.Api/Program.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using LabAPI.Api.Middlewares;
 using LabAPI.Application.Common.Extensions;
 using LabAPI.Infrastructure.Extensions;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +15,32 @@ builder.Services.AddControllers();
 builder.Services.AddTransient<ErrorHandlingMiddleware>();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
