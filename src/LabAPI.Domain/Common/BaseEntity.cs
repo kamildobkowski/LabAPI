d8a4cadc3ffd52d001f3095575be13c9d441a42b
kamildobkowski <kamil.dobkowski.03@gmail.com>
2024-03-26 13:09:17 +0100

    Fix created date

--- src/LabAPI.Domain/Common/BaseEntity.cs
+++ src/LabAPI.Domain/Common/BaseEntity.cs
@@ -6,6 +6,6 @@ namespace LabAPI.Domain.Common;
 public abstract class BaseEntity
 {
 	[JsonProperty("id")] public virtual string Id { get; set; } = Guid.NewGuid().ToString();
-	public DateTime CreatedAt { get; set; } = DateTime.Now;
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 	public DateTime? ModifiedAt { get; set; }
 }
