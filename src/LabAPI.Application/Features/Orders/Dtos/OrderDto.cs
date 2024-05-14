d60efb5d61c68423740f8cba585db9f4a57d8d5a
kamildobkowski <kamil.dobkowski.03@gmail.com>
2024-05-14 20:54:56 +0200

    Expand OrderDto to deliver test details rather than plain results

--- src/LabAPI.Application/Features/Orders/Dtos/OrderDto.cs
+++ src/LabAPI.Application/Features/Orders/Dtos/OrderDto.cs
@@ -1,15 +1,33 @@
 using LabAPI.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace LabAPI.Application.Features.Orders.Dtos;
 
 public sealed record OrderDto
 {
 	public string OrderNumber { get; init; } = string.Empty;
-	public string FullName { get; set; } = string.Empty;
+	public string FullName { get; init; } = string.Empty;
 	public string? Pesel { get; init; }
 	public DateOnly DateOfBirth { get; init; }
 	public string? Sex { get; init; }
-	public string Status { get; set; } = string.Empty;
+	public string Status { get; init; } = string.Empty;
 	public Address? Address { get; init; }
-	public Dictionary<string, Dictionary<string, string>?> Results { get; init; } = null!;
+	public List<TestsWithResultsDto> Tests { get; init; } = [];
+
+	public sealed record TestsWithResultsDto
+	{
+		public string ShortName { get; init; } = string.Empty;
+		public string Id { get; init; } = string.Empty;
+		public string Name { get; init; } = string.Empty;
+		public List<MarkerWithResultDto> Markers { get; set; } = [];
+		public sealed record MarkerWithResultDto
+		{
+			public string Name { get; init; } = string.Empty;
+			public string? Result { get; init; }
+			public string? LowerNorm { get; init; }
+			public string? HigherNorm { get; init; }
+			public string? Unit { get; init; }
+			public string? ShortName { get; init; }	
+		}
+	}
 }
--- src/LabAPI.Application/Features/Orders/Queries/GetOrderQuery.cs
+++ src/LabAPI.Application/Features/Orders/Queries/GetOrderQuery.cs
@@ -1,18 +1,45 @@
 using AutoMapper;
 using LabAPI.Application.Features.Orders.Dtos;
+using LabAPI.Domain.Exceptions;
 using LabAPI.Domain.Repositories;
 using MediatR;
 
 namespace LabAPI.Application.Features.Orders.Queries;
 
 public sealed record GetOrderQuery(string OrderNumber) : IRequest<OrderDto>;
-internal sealed class GetOrderQueryHandler(IOrderRepository repository, IMapper mapper)
+internal sealed class GetOrderQueryHandler(IOrderRepository repository, ITestRepository testRepository, IMapper mapper)
 	: IRequestHandler<GetOrderQuery, OrderDto>
 {
 	public async Task<OrderDto> Handle(GetOrderQuery request, CancellationToken cancellationToken)
 	{
 		var entity = await repository.GetAsync(r => r.OrderNumber == request.OrderNumber);
+		if (entity is null)
+			throw new NotFoundException();
 		var dto = mapper.Map<OrderDto>(entity);
+		foreach (var i in entity.Results.Keys)
+		{
+			var test = await testRepository.GetAsync(r=>r.ShortName == i);
+			var testDto = new OrderDto.TestsWithResultsDto
+			{
+				Id = test!.Id,
+				Name = test.Name,
+				ShortName = test.ShortName
+			};
+			foreach (var marker in test.Markers)
+			{
+				var markerDto = new OrderDto.TestsWithResultsDto.MarkerWithResultDto
+				{
+					Name = marker.Name,
+					LowerNorm = marker.LowerNorm.ToString(),
+					HigherNorm = marker.HigherNorm.ToString(),
+					Unit = marker.Unit,
+					ShortName = marker.ShortName,
+					Result = entity.Results[i]![marker.ShortName]
+				};
+				testDto.Markers.Add(markerDto);
+			}
+			dto.Tests.Add(testDto);
+		}
 		return dto;
 	}
 } 
